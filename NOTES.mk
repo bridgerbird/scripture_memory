658e260ec9c6737acf9276a680f108046e498299
bridgerbird <bridgerjbird@gmail.com>
2026-01-25 19:03:57 -0700

    Added NOTES and cleaned up main

---
+++ NOTES.mk
@@ -0,0 +1,7 @@
+# Instead of saving different text alterations in temp_scripture, have the way it is displayed change dynamically according to the following options:
+# 1) Percentage Slider: takes out words by percentage at equal intervals, for example 50% would be every other word.
+# 2) Word Size: takes out all words of a certain size, for example you could take out all words 4 letters or shorter, or 7 letters or greater, etc.
+# 3) Line Mastery: only displays the first 3 words of each line
+# 4) Customization: you select which words or sections to remove and save it as an option
+#       - you can create as many customizations per verse as you'd like.
+#		- this is setup as chunks currently
